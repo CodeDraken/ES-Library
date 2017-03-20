a774860c4243e7d20ef75a172adcb5a4bce816ee
CodeDraken <codedraken@gmail.com>
2017-03-20 13:34:33 -0700

    Redesign ship page initial setup

--- src/components/ShipPage.js
+++ src/components/ShipPage.js
@@ -2,76 +2,77 @@ import React from 'react';
 import PrevButton from './PrevButton';
 
 const ShipPage = ({ship, match}) => {
+  const { layout, outfits } = ship,
+    attr = ship.attributes,
+    weapon = attr.weapon;
+
   let sprite;
   console.log('ship: ', ship)
   try {
     sprite = require(`./../img/${(ship.sprite).replace(/ /g, '_')}.png`);
-  } catch(err) {
+  } catch (err) {
     console.warn(err)
     try {
       sprite = require(`./../img/${(ship.sprite).replace(/ /g, '_')}-0.png`);
-    } catch(err) {
+    } catch (err) {
       sprite = '';
     }
-    
   }
-  
+
   return (
     <section>
-
-    {match && match.url ? <PrevButton url={match.url} /> : ''}
-
-      <h3>
-        {ship.name}
-      </h3>
-
+      { match && match.url ? <PrevButton url={ match.url } /> : '' }
       <div className="row">
 
-        <div className="col s12 m8">
-          <p>{ship.description}</p>
-        </div>
+        <div className="col s12">
+          <h3 className="ship__title center-align">{ ship.name }</h3>
+          <p className="ship__description">{ ship.description }</p>
+          <div className="row">
 
-        <div className="col s4">
-          <img src={sprite} alt={`${ship.name} missing image`}/>
+            <div className="col s12 m3 l2">
+              <table className="bordered highlight">
+                <thead>
+                  <tr>
+                    <th data-field="attribute">Attribute</th>
+                    <th data-field="value"> Value</th>
+                  </tr>
 
-          <table className="bordered highlight">
-            <thead>
-              <tr>
-                  <th data-field="attribute">Attribute</th>
-                  <th data-field="value"> Value</th>
-              </tr>
-            </thead>
+                  <tr>
+                    <td><strong>Cost</strong></td>
+                    <td>{attr.cost.toString()}</td>
+                  </tr>
 
-            <tbody>
+                </thead>
+              </table>
+            </div>
 
-              {Object.keys(ship.attributes).map((key) => {
-                if(key !== 'weapon') {
-                  return (
-                    <tr key={key}>
-                      <td><strong>{key}</strong></td>
-                      <td>{(ship.attributes[key]).toString()}</td>
-                    </tr>
-                  );
-                } else {
-                  return Object.keys(ship.attributes.weapon).map((key) => {
-                    return (
-                      <tr key={key}>
-                        <td><strong>{key}</strong></td>
-                        <td>{(ship.attributes.weapon[key]).toString()}</td>
-                      </tr>
-                    );
-                  });
-                }
 
-              })}
+            <div className="col s12 m6 l8">
+              <img className="ship__sprite" src={ sprite } alt={ `${ship.name} missing image` } />
+            </div>
 
-            </tbody>
-        </table>
-          
+
+            <div className="col s12 m3 l2">
+              <table className="bordered highlight">
+                <thead>
+                  <tr>
+                    <th data-field="attribute">Attribute</th>
+                    <th data-field="value"> Value</th>
+                  </tr>
+
+                  <tr>
+                    <td><strong>Cost</strong></td>
+                    <td>{attr.cost.toString()}</td>
+                  </tr>
+
+                </thead>
+              </table>
+            </div>
+
+          </div>
         </div>
 
       </div>
-
     </section>
   );
 };
@@ -105,8 +106,33 @@ ShipPage.defaultProps = {
     layout: {},
     name: 'Loading ship...',
     outfits: [],
-    sprite: 'ship/sparrow'
+    sprite: ''
   }
 };
 
 export default ShipPage;
+
+
+
+// <tbody>
+//   {Object.keys(ship.attributes).map((key) => {
+//     if(key !== 'weapon') {
+//       return (
+        // <tr key={key}>
+        //   <td><strong>{key}</strong></td>
+        //   <td>{(ship.attributes[key]).toString()}</td>
+        // </tr>
+//       );
+//     } else {
+//       return Object.keys(ship.attributes.weapon).map((key) => {
+//         return (
+//           <tr key={key}>
+//             <td><strong>{key}</strong></td>
+//             <td>{(ship.attributes.weapon[key]).toString()}</td>
+//           </tr>
+//         );
+//       });
+//     }
+
+//   })}
+// </tbody>
